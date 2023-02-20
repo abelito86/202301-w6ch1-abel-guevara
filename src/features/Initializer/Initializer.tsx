f450d269a12053b4b4aeba0ff142fa658709caa2
Abel <abelguevaralanda@yahoo.es>
2023-02-20 21:43:37 +0100

    add initializer feature

--- src/features/Initializer/Initializer.tsx
+++ src/features/Initializer/Initializer.tsx
@@ -1,15 +1,28 @@
 import { useAppDispatch, useAppSelector } from "../../app/hooks";
-import { randomAsync, seletcRandom } from "./initializer.Slice";
+import {
+  randomAsync,
+  selectRandomStatus,
+  seletcRandom,
+} from "./initializer.Slice";
+import "./Initializer.css";
 
 const Random = () => {
   const randomNum = useAppSelector(seletcRandom);
+  const loadingNum = useAppSelector(selectRandomStatus);
   const dispatch = useAppDispatch();
 
   return (
     <div>
       <p>{randomNum}</p>
-      <button className="random-button" onClick={() => dispatch(randomAsync())}>
-        Init From API
+      <button
+        onClick={() => dispatch(randomAsync())}
+        disabled={loadingNum !== "idle"}
+      >
+        {loadingNum !== "idle" ? (
+          <span className="lds-dual-ring"></span>
+        ) : (
+          <span>Init From API</span>
+        )}
       </button>
     </div>
   );
